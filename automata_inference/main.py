695734c05e4764b0ee5f47c0a7751702175f3a72
Dominik <dominik.geissler@rwth-aachen.de>
2026-09-02 16:36:54 +0200

    linter

--- automata_inference/main.py
+++ automata_inference/main.py
@@ -7,16 +7,21 @@ from automata_inference.parser.parser import parse
 from automata_inference.visualizer import visualize
 
 
+def main(program_path: str, visualize_posterior: bool):
+    """Main method of the program.
 
-def main(program_path, visualize_posterior):
+    Args:
+        program_path (str): The path to the program file to be analyzed.
+        visualize_posterior (bool): Indicates whether the normalized posterior should be visually depicted.
+    """
     program = parse(program_path)
-    
+
     print()
     print("----------------------------------")
     print("-       Starting Analysis        -")
     print("----------------------------------")
     print()
-    
+
     input_pga = PGAFactory.one((program.variables | {"1"}))
     start = time.time()
     out = program.apply_semantics(input_pga)
@@ -31,26 +36,22 @@ def main(program_path, visualize_posterior):
         print(f"Output of '{program.query}': {program.evaluate_query(out)}")
     if visualize_posterior:
         visualize(out, view=True)
-    
-    
+
 
 def create_parser():
-    parser = ArgumentParser(
-        description="Anaylsis of discrete probabilistic programs."
-    )
+    """Creates and returns the parser of the CLI arguments."""
+    argument_parser = ArgumentParser(description="Anaylsis of discrete probabilistic programs.")
 
-    parser.add_argument(
-        "program_path",
-        help="Path to the program file."
-    )
-    
-    parser.add_argument(
+    argument_parser.add_argument("program_path", help="Path to the program file.")
+
+    argument_parser.add_argument(
         "--visualize-posterior",
         action="store_true",
-        help="If set, renders the automaton representation of the normalized posterior distribution."
+        help="If set, renders the automaton representation of the normalized posterior distribution.",
     )
 
-    return parser
+    return argument_parser
+
 
 if __name__ == "__main__":
     parser = create_parser()
